f755ba6cdfbdbbebe36c89687d836c0957a150c6
zeljkosoko <zex.sokolovic@gmail.com>
2020-09-03 12:33:08 +0200

    hotel - sliding reviews final

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var sliders = document.querySelectorAll('.slide')
 var dot1 = document.querySelector('.dot1');
 var dot2 = document.querySelector('.dot2');
 var dot3 = document.querySelector('.dot3');
+var dots = document.querySelectorAll('.dot');
 
 var index = 0;
 
@@ -10,12 +11,18 @@ hideSliders(index);
 
 dot1.addEventListener('click', function(){
     hideSliders(0);
+    removeActive();
+    this.classList.add('active-dot');
 });
 dot2.addEventListener('click', function(){
     hideSliders(1);
+    removeActive();
+    this.classList.add('active-dot');
 });
 dot3.addEventListener('click', function(){
     hideSliders(2);
+    removeActive();
+    this.classList.add('active-dot');
 });
 
 function hideSliders(num){
@@ -23,4 +30,10 @@ function hideSliders(num){
         sliders[i].style.display = "none";
     }
     sliders[num].style.display = "block";
+}
+
+function removeActive(){
+    for(var i = 0; i<dots.length; i++){
+        dots[i].classList.remove("active-dot");
+    }
 }
